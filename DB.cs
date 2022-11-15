72a783b5c4da0643654ea856c26b0d8da1843fee
Daitarus <chajkovskij_99@mail.ru>
2022-11-15 14:54:03 +0300

    add create DB with script

--- DB.cs
+++ DB.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using System.Text;
 
 namespace RepositoryDB
 {
@@ -7,11 +8,36 @@ namespace RepositoryDB
         private static string? connectionString;
         public static string ConnectionString { set { connectionString = value; } }
 
+        public DB()
+        {
+            Database.EnsureCreated();
+        }
+        public DB(string script)
+        {
+            if (Database.EnsureCreated())
+            {
+                Database.ExecuteSqlRaw(script);
+            }
+        }
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             if (connectionString != null)
             {
                 optionsBuilder.UseNpgsql(connectionString);
+            }
+        }
+
+        public static bool CheckDB(string script)
+        {
+            try
+            {
+                using (new DB(script)) { }
+                return true;
+            }
+            catch
+            {
+                return false;
             }
         }
 
@@ -26,6 +52,27 @@ namespace RepositoryDB
             {
                 return false;
             }
+        }
+
+        public static string? GetSqlScript(string file)
+        {
+            string sqlScript = null;
+
+            if (file != null || file != "")
+            {
+                FileInfo fileInfo = new FileInfo(file);
+                if (fileInfo.Exists)
+                {
+                    byte[] sqlScriptBytes = new byte[fileInfo.Length];
+                    using (FileStream fstream = System.IO.File.Open(fileInfo.FullName, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+                    {
+                        fstream.Read(sqlScriptBytes);
+                    }
+                    sqlScript = Encoding.UTF8.GetString(sqlScriptBytes);
+                }
+            }
+
+            return sqlScript;
         }
     }
 }
